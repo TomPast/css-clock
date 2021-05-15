276512b1b026d099200ef4904b2e0c3acd592bbf
Dalo <pastor.tom.etu@gmail.com>
2021-05-15 19:09:42 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react'
+import invert from 'invert-color';
+import Particles from 'react-particles-js';
+import './App.css'
+
+function App() {
+  const [clock, setClock] = useState(new Date())
+  const [HEXclock, setHEXClock] = useState("#000000")
+
+  useEffect(() => {
+    const interval = setInterval(
+      () => getTime(),
+      1000
+    );
+ 
+    return () => {
+      clearInterval(interval);
+    }
+  }, []);
+
+  const getTime = () => {
+    const d = new Date();
+    setClock(d)
+    getHEXFromDate(d)
+  }
+
+  const getHEXFromDate = (date) => {
+    const clockText = date.toLocaleTimeString().split(":")
+    setHEXClock("#"+clockText[0]+clockText[1]+clockText[2])
+  }
+
+
+  return (
+    <div className="App" style={{backgroundColor: HEXclock}}>
+      <h1 className="title" style={{color: invert(HEXclock, true)}}>{HEXclock}</h1>
+    </div>
+  )
+}
+
+export default App
